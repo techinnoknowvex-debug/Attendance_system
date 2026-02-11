b8cbaeb169cc6f31664e6dce2798258ee2538c11
madhan-890 <madhanthota60@gmail.com>
2026-02-11 16:06:47 +0530

    Reapply gitignore and remove tracked env files

--- backend/models/supabaseModels.js
+++
@@ -1,9 +0,0 @@
-// Supabase table name constants
-// This file replaces the Mongoose models with table name references
-
-module.exports = {
-  EMPLOYEES_TABLE: 'employees',
-  ATTENDANCE_TABLE: 'attendance',
-  LOP_TABLE: 'lop'
-};
-
